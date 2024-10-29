fbefcb27b602684916f99cd98470b96c5fc4ee3b
Justin <justinsalsan11@gmail.com>
2024-10-28 23:39:16 -0500

    SistemaListar

--- RegistroDeEventos/src/Listar/SistemaPagoList.java
+++ RegistroDeEventos/src/Listar/SistemaPagoList.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Listar;
 
-/**
- *
- * @author puert
- */
+import Clases.SistemaPago;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaPagoList {
-    
+    List<SistemaPago> listaSistemaPagos = new ArrayList<>();  
+  public void agregar (SistemaPago p){
+      listaSistemaPagos.add(p);
+  }
+  public void quitar (SistemaPago p){
+       listaSistemaPagos.remove(p);
+  }
+  public SistemaPago obtener (int pag){
+      return  listaSistemaPagos.get(pag);
+  }
+  public List <SistemaPago> listar(){
+      return this.listaSistemaPagos;
+  }
 }
